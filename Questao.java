62c9337ba4680315fea6700fce7ce81a01c7cd6e
Eeveee-vitoria <evellynnunesofc@gmail.com>
2025-03-27 21:09:04 -0300

    Update Questao.java

--- Questao.java
+++ Questao.java
@@ -1,23 +1,22 @@
 import java.util.Scanner;
-
+\\ Classe da questao onde estou atribuindo para que o main funcione 
 public class Questao {
     private String pergunta;
     private String[] opcoes;
     private String respostaCorreta;
+
 
     public Questao(String pergunta, String[] opcoes, String respostaCorreta) {
         this.pergunta = pergunta;
         this.opcoes = opcoes;
         this.respostaCorreta = respostaCorreta;
     }
-
     public void exibirQuestao() {
         System.out.println(pergunta);
         for (int i = 0; i < opcoes.length; i++) {
             System.out.println((char) ('a' + i) + ") " + opcoes[i]);
         }
     }
-
     public boolean verificarResposta(String respostaUsuario) {
         return respostaUsuario.equalsIgnoreCase(respostaCorreta);
     }
